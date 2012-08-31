670ecfb9cc4eb629a7394453470dcc65626ae482
Michel Martens <michel@soveran.com>
2012-08-31 19:31:01 -0300

    Vendor clap.

---
+++ lib/cutest/vendor/clap.rb
@@ -0,0 +1,46 @@
+class Clap
+  attr :argv
+  attr :opts
+
+  def self.run(args, opts)
+    new(args, opts).run
+  end
+
+  def initialize(argv, opts)
+    @argv = argv.dup
+    @opts = opts
+  end
+
+  def run
+    args = []
+
+    while argv.any?
+
+      item = argv.shift
+      flag = opts[item]
+
+      if flag
+
+        # Work around lambda semantics in 1.8.7.
+        arity = [flag.arity, 0].max
+
+        # Raise if there are not enough parameters
+        # available for the flag.
+        if argv.size < arity
+          raise ArgumentError
+        end
+
+        # Call the lambda with N items from argv,
+        # where N is the lambda's arity.
+        flag.call(*argv.shift(arity))
+      else
+
+        # Collect the items that don't correspond to
+        # flags.
+        args << item
+      end
+    end
+
+    args
+  end
+end
