630f77a4c1fcbd29dafaddfa3bf200357319d6f5
Michel Martens <michel@soveran.com>
2012-10-09 11:53:37 -0300

    Release.

--- lib/cutest.rb
+++ lib/cutest.rb
@@ -1,6 +1,6 @@
 class Cutest
   unless defined?(VERSION)
-    VERSION = "1.2.0.rc3"
+    VERSION = "1.2.0"
     FILTER = %r[/(ruby|jruby|rbx)[-/]([0-9\.])+]
     CACHE = Hash.new { |h, k| h[k] = File.readlines(k) }
   end
